b18873f6a8aaac1d8fcf7221c792bf91af04f239
yashrewa <yashrewa00@gmail.com>
2024-03-03 03:51:08 +0530

    Application is live now!

--- client/src/components/Utils/backendLink.tsx
+++ client/src/components/Utils/backendLink.tsx
@@ -1,2 +1,2 @@
-export const expressBackend = 'http://localhost:3000/user'
-export const socketBakcend = 'http://localhost:3000'
+export const expressBackend = 'https://bl-test-task-mern-stack.onrender.com/user'
+export const socketBakcend = 'https://bl-test-task-mern-stack.onrender.com'
